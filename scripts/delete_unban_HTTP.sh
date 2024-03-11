c85076e56661b25e47cf374411691fcaa073e02f
sebuxor <seb57@protonmail.com>
2024-03-11 13:31:46 +0000

    v2

---
+++ scripts/delete_unban_HTTP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables de connexion à la base de données
+DB_NAME="ip_database"
+DB_USER="fail2ban_user"
+DB_PASSWORD="yourpassword"
+DB_HOST="localhost"
+
+# Adresse IP à débannir
+IP=$1
+
+# Retire l'adresse IP de la zone blockzone dans Firewalld
+firewall-cmd --permanent --zone=blockzoneHTTP --remove-source=$IP
+firewall-cmd --reload
+
+# Supprime l'entrée de l'adresse IP de la base de données
+SQL_COMMAND="DELETE FROM ip_bans WHERE ip_address = '$IP';"
+
+# Exécute la commande SQL via psql
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -d $DB_NAME -h $DB_HOST -c "$SQL_COMMAND"
---
+++ scripts/insert-ban.sql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DB_NAME="fail2ban_db"
+DB_USER="fail2ban_user"
+DB_PASSWORD="********"
+DB_HOST="localhost"
+
+# Collecte des informations via whois (comme avant)
+WHOIS_OUTPUT=$(whois $1)
+COUNTRY=$(echo "$WHOIS_OUTPUT" | grep -i "country:" | head -n 1 | awk '{print $NF}')
+CITY=$(echo "$WHOIS_OUTPUT" | grep -i "city:" | head -n 1 | awk '{print $NF}')
+COUNTRY=${COUNTRY:-Unknown}
+CITY=${CITY:-Unknown}
+
+# Ajout du type de connexion comme argument $3
+CONNECTION_TYPE=$3
+
+# Commande SQL ajustée pour inclure le type de connexion
+SQL_COMMAND="INSERT INTO ip_bans (ip_address, country, city, ban_time, jail, connection_type) VALUES ('$1', '$COUNTRY', '$CITY', now(), '$2', '$CONNECTION_TYPE');"
+
+# Exécute la commande SQL via psql
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -d $DB_NAME -h $DB_HOST -c "$SQL_COMMAND"
